544735243c9b971f0d7d65cff0b1d1ea8491424d
Kaida Masaki <kaida@adadevelopersacademy.org>
2021-10-14 14:54:08 -0700

    Add open addressing scaffold + tests

---
+++ hashtable/open_addressing.py
@@ -0,0 +1,41 @@
+class OpenAddressingTable:
+    def __init__(self, table_size=4):
+        self.size = 0
+        self.table = []
+        for i in range(table_size):
+            self.table.append([])
+
+    def insert(self, key, value):
+        """
+        Insert `(key, value)` based on the hashed value of `key`.
+        """
+
+        # TODO: Try to insert into self.table
+
+        # If successful, increment.
+        self.size += 1
+
+    def get(self, key, default=None):
+        """
+        Return the value stored with `key` or `default` if it is not there.
+        """
+        start_index = hash(key) % len(self.table)
+        index = start_index
+
+        if not self.table[index]:
+            return default
+
+        # We need to do this once outside of the loop before we get started.
+        current_key, value = self.table[index]
+        if current_key == key:
+            return value
+        index += 1 % len(self.table)
+
+        while index != start_index:
+            current_key, value = self.table[index]
+            if current_key == key:
+                return value
+            index += 1 % len(self.table)
+
+        # Return default if we don't find the key.
+        return default
---
+++ tests/test_open_addressing.py
@@ -0,0 +1,30 @@
+import random
+
+from hashtable.open_addressing import OpenAddressingTable
+
+def test_insert():
+    nums = []
+    # Get a unique random number.
+    #
+    # This is mostly me being paranoid about bad luck.
+    def get_unique_num():
+        num = random.randint(-10000, 10000)
+        while num in nums:
+            num = random.randint(-10000, 10000)
+        nums.append(num)
+        return num
+
+    table = OpenAddressingTable()
+
+    for size in range(1, 5):
+        key = get_unique_num()
+        val = get_unique_num()
+
+        table.insert(key, val)
+
+        assert table.size == size
+        assert table.get(key) == val
+
+    # Check for missing keys.
+    assert table.get(get_unique_num()) == None
+    assert table.get(get_unique_num(), "missing") == "missing"
